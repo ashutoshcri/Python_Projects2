1bd21b927ede7841cfe5bf2a9e0765e6914a66e7
Ashutosh Saxena <ashutoshsaxena788@gmail.com>
2020-03-30 12:11:15 +0530

    Merge remote-tracking branch 'origin2/master'
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	venv/Lib/site-packages/setuptools-40.8.0-py3.8.egg
    #	venv/Scripts/Activate.ps1
    #	venv/Scripts/activate
    #	venv/Scripts/activate.bat
    #	venv/Scripts/easy_install-3.8-script.py
    #	venv/Scripts/easy_install-script.py
    #	venv/Scripts/pip-script.py
    #	venv/Scripts/pip3-script.py
    #	venv/Scripts/pip3.8-script.py
    #	venv/Scripts/python.exe
    #	venv/Scripts/pythonw.exe
    #	venv/pyvenv.cfg

--- script1.py
+++ script1.py
@@ -1,4 +1,28 @@
-dict1={1:'hello',2:'good',3:'bye',4:'neo'}
-for k,v in dict1.items():
-    print('\nThe key is',k)
-    print('The value is',v.title())
+import json
+
+def get_stored_username():
+    filename='username.json'
+    try:
+        with open(filename) as f:
+            usr=json.load(f)
+    except FileNotFoundError:
+        pass
+    else:
+        return usr
+
+def get_new_username():
+    usr=input('What is your name? ')
+    filename='username.json'
+    with open(filename, 'w') as f:
+        json.dump(usr, f)
+    return usr
+
+def greet_user():
+    usr=get_stored_username()
+    if usr:
+        print(f"Welcome back, {usr}!")
+    else:
+        usr=get_new_username()
+        print(f"We'll remember when you come back, {usr}!")
+
+greet_user()
