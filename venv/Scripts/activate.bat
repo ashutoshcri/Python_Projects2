cc968664c9b9af2392639f7628f46ac4df177429
Ashutosh Saxena <ashutoshsaxena788@gmail.com>
2020-07-19 23:39:16 +0530

    Merge remote-tracking branch 'origin2/master'
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	venv/Lib/site-packages/setuptools-40.8.0-py3.8.egg
    #	venv/Scripts/Activate.ps1
    #	venv/Scripts/activate
    #	venv/Scripts/activate.bat
    #	venv/Scripts/easy_install-3.8-script.py
    #	venv/Scripts/easy_install-script.py
    #	venv/Scripts/pip-script.py
    #	venv/Scripts/pip3-script.py
    #	venv/Scripts/pip3.8-script.py
    #	venv/Scripts/python.exe
    #	venv/Scripts/pythonw.exe
    #	venv/pyvenv.cfg

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Python Programs\venv
+set VIRTUAL_ENV=C:\Python_Programs\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
