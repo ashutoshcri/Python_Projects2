64448ae4c3b8be577d8c6ca76d0cd9af59521ee2
ashutoshcri <ashutoshsaxena788@gmail.com>
2020-10-17 03:15:25 +0530

    Merge remote-tracking branch 'origin2/master'
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	venv/Lib/site-packages/setuptools-40.8.0-py3.8.egg
    #	venv/Scripts/Activate.ps1
    #	venv/Scripts/activate
    #	venv/Scripts/activate.bat
    #	venv/Scripts/easy_install-3.8-script.py
    #	venv/Scripts/easy_install-script.py
    #	venv/Scripts/pip-script.py
    #	venv/Scripts/pip3-script.py
    #	venv/Scripts/pip3.8-script.py
    #	venv/Scripts/python.exe
    #	venv/Scripts/pythonw.exe
    #	venv/pyvenv.cfg

---
+++ scr1.py
@@ -0,0 +1,14 @@
+import os
+
+file1=open('Doc.txt','r')
+read1=file1.read()
+
+# count=0
+# st=''
+# for ele in read1:
+#     if(ele>='0' and ele<='9'):
+#         count+=1
+#         st=st+ele
+# print(count)
+# print(st)
+
